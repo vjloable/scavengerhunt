06e287b99f4026558b2d23bd00cd1edb2fe84f23
Eagl3ye <vincejaphethloable@gmail.com>
2023-09-09 00:32:04 +0800

    style: finalize color palette

--- lib/misc/palette.dart
+++ lib/misc/palette.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
 class Palette {
-  static Color primaryColor = const Color(0xFFF9CA38);
-  static Color secondaryColor =  const Color(0xFF8bca60);
-  static Color bgColor = const Color(0xFF2F2F2F);
+  static Color primaryColor = const Color(0xFF24E1BB);
+  static Color secondaryColor =  const Color(0xFF19BCBB);
+  static Color primaryAccentColor = const Color(0xFF0074ff);
+  static Color secondaryAccentColor = const Color(0xFF0074ff);
+  static Color primaryBright = const Color(0xFFFFFFFF);
+  static Color secondaryBright = const Color(0xFFEAEBEF);
+  static Color primaryDark = const Color(0xFF000000);
+  static Color secondaryDark = const Color(0xFF323232);
+  static List<Color> primaryGradient = [const Color(0xFF0B0B0B), const Color(0xFF12201F), const Color(0xFF042C23)];
+  static List<Color> secondaryGradient = [const Color(0x44000000), const Color(0x44323232)];
 }
